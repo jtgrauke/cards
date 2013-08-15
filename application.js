8b69bc0ef1df2bde62956f26cba34496446d4d2d
JT Grauke <jtgrauke@gmail.com>
2013-08-15 09:50:52 -0700

    Removing the feedback

--- application.js
+++ application.js
@@ -7,36 +7,26 @@ function pickOne(){
     var choice = $(this).attr('cardNum');
         distance = Math.abs(choice - chosenOne);
     if (distance == 0) {
-        $('.feedback').text("Nice! You got it.").css({color:"#333"});
+        $('.feedback').text("Nice! You got it.").css({color:"#333"}).show();
         $('.reset').show();
         $(this).css({'background-color':'#5CD357'});
     } else if (distance == 1) {
-        $('.feedback').text("SO CLOSE!");
         $(this).css({'background-color':'#FF5757'});
     } else if (distance == 2) {
-        $('.feedback').text("Close.");
         $(this).css({'background-color':'#FF8657'});
     } else if (distance == 3) {
-        $('.feedback').text("Close but no cigar.");
         $(this).css({'background-color':'#FFCB47'});
     } else if (distance == 4) {
-        $('.feedback').text("Hmmm, yeah no.");
         $(this).css({'background-color':'#FFDC83'});
     } else if (distance == 5) {
-        $('.feedback').text("Ooo, off by about ' — ' much.");
         $(this).css({'background-color':'#92CCE7'});
     } else if (distance == 6) {
-        $('.feedback').text("Yikes, nope.");
         $(this).css({'background-color':'#5EAFD5'});
     } else if (distance == 7) {
-        $('.feedback').text("Waaay off.");
         $(this).css({'background-color':'#4897BD'});
     } else {
-        $('.feedback').text("Try the other side!");
         $(this).css({'background-color':'#2C72A2'});
     }
-
-    $('.feedback').show();
 }
 
 function reset(){
